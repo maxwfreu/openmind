f0ba5f1a49baa273c99b292e543779423a35970e
Max Freundlich <maxwfreu@stanford.edu>
2017-03-07 22:22:29 -0800

    article text loads in

--- src/article.js
+++ src/article.js
@@ -2,15 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './style/articleStyle.css'
-
+var $ = require ('jquery')
 const PageView = React.createClass({
+  componentDidMount: function() {
+    this.loadArticle();
+  },
   render () {
     return (
       <div className="row">
-
       <div className="col-md-8">
-        <h2> Cool Title </h2>
-        <h5> This is a sub title </h5>
+        <h2> WikiLeaks Releases Trove of Alleged C.I.A. Hacking Documents </h2>
+        <h4> This is a subtitle, wow </h4>
         <img src={require('./images/dictator.jpg')} alt="trump" style={{width: 100 + '%'}}/>
         <div className="infoContainer">
           <div className="ovalViewOverlay"><div className="moneyInOvalView"> 7 $</div></div>
@@ -24,14 +26,7 @@ const PageView = React.createClass({
             <span className="glyphicon glyphicon-share-alt"></span> Share
           </button>
         </div>
-        <p style={{paddingTop: 5 + 'px'}}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.
-        </p>
+        <div id="articleContent" style={{paddingTop: 5 + 'px'}}></div>
       </div>
       <div className="col-md-4">
         <div style={{paddingTop:40 +'px'}}>
@@ -42,9 +37,19 @@ const PageView = React.createClass({
         </div>
       </div>
     </div>
+  )
+},
+loadArticle: function() {
+  $(function() {
+  $("#articleContent").load("articleText/wikiLeaks.html");
+  });
+  // render the buy button with jQuery
+  // $.get("articleText/wikiLeaks.html", function(txt) {
+  //   $('#articleContent').text(txt)
+  // })
+  //document.getElementById("articleContent").innerHTML='<object type="text/html" data="articleText/wikiLeaks.html" ></object>';
 
-    )
-  }
+}
 })
 
 const ArticleView = React.createClass({
