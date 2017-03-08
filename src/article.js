831a39a46f8c53cbd33f7b2583db6aa6321a8bb9
Max Freundlich <maxwfreu@stanford.edu>
2017-03-07 22:53:48 -0800

    article side view and code cleanup

--- src/article.js
+++ src/article.js
@@ -3,17 +3,32 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './style/articleStyle.css'
 var $ = require ('jquery')
-const PageView = React.createClass({
-  componentDidMount: function() {
-    this.loadArticle();
-  },
-  render () {
+
+var sideData = [{title: "North Korea Tensions Pose Early, and Perilous, Test for Trump", url:'./images/nkorea.jpg'}, {title:"Dutch Fear Russian Meddling, and U.S. Cash, in Election ", url:'./images/dutch.jpg'}, {title:"Trump is a madman", url:'./images/dictator.jpg'}];
+//NOTE this will only be one thumbnail
+const SideArticle = React.createClass({
+  render (){
+    var items = sideData.map(function(item, i) {
+         return(
+           <div className="thumbnail" key={i}>
+             <img src={require(item.url)} alt="trump"/>
+             <div className="caption">
+               <h5> {item.title} </h5>
+             </div>
+           </div>
+         )
+    });
+    return (<div>{items}</div>);
+  }
+})
+
+const MainArticle = React.createClass({
+  render(){
     return (
-      <div className="row">
-      <div className="col-md-8">
+      <div>
         <h2> WikiLeaks Releases Trove of Alleged C.I.A. Hacking Documents </h2>
-        <h4> This is a subtitle, wow </h4>
-        <img src={require('./images/dictator.jpg')} alt="trump" style={{width: 100 + '%'}}/>
+        <h5> By SCOTT SHANE, MATTHEW ROSENBERG and ANDREW W. LEHREN &middot; MARCH 7, 2017 </h5>
+        <img src={require('./images/wikileaks.jpg')} alt="trump" style={{width: 100 + '%'}}/>
         <div className="infoContainer">
           <div className="ovalViewOverlay"><div className="moneyInOvalView"> 7 $</div></div>
           <i className="material-icons icon" style={{fontSize:36 + 'px'}}>people</i>
@@ -28,12 +43,22 @@ const PageView = React.createClass({
         </div>
         <div id="articleContent" style={{paddingTop: 5 + 'px'}}></div>
       </div>
+    )
+  }
+})
+const PageView = React.createClass({
+  componentDidMount: function() {
+    this.loadArticle();
+  },
+  render () {
+    return (
+      <div className="row">
+      <div className="col-md-8">
+        <MainArticle />
+      </div>
       <div className="col-md-4">
-        <div style={{paddingTop:40 +'px'}}>
-          <h3> Some Other Article</h3>
-          <h3> Some Other Article</h3>
-          <h3> Some Other Article</h3>
-          <h3> Some Other Article</h3>
+        <div className="articleList">
+          <SideArticle />
         </div>
       </div>
     </div>
