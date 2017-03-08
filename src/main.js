f397e78845e840649cf6757fbd68c9f56fd74342
Max Freundlich <maxwfreu@stanford.edu>
2017-03-07 19:48:53 -0800

    reworked bootstrap, added money oval

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router'
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './style/articlecards.css';
 
 const NewsCard = React.createClass({
   render () {
     return (
-      <div className="col-xs-6 col-md-4">
-        <div className="w3-card-12">
-          <img src={require('./images/dictator.jpg')} alt="trump" style={{width: 100 + '%'}}/>
-          <div className="w3-container w3-center">
-            <p style={{paddingTop: 5 + 'px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="col-sm-6 col-md-4">
+        <div className="thumbnail">
+          <div className="oval_parent">
+            <div className="oval"><div className="moneyInOval"> 7 $ </div></div>
+          </div>
+          <img src={require('./images/dictator.jpg')} alt="trump"/>
+          <div className="caption">
+            <h3> Article About Trump </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
           </div>
         </div>
       </div>
